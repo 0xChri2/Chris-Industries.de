52b793a4098ae770464d6bb5810f8f32bb46d9e3
ImChri2 <chris@chris-industries.de>
2022-01-22 23:41:26 +0100

    first folders and beginning of index.php

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Chris Industries</title>
+        <meta charset="utf-8"/>
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="stlye.css">
+        <script src="http://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
+        <>
+    </head>
+
+        <body>
+        
+        </body>
+</html>
