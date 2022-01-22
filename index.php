e75d0a5147fbaf1023de9c8fe3f7b482cf2d91bd
ImChri2 <chris@chris-industries.de>
2022-01-22 23:58:26 +0100

    index.php adding heading

--- index.php
+++ index.php
@@ -4,12 +4,15 @@
         <title>Chris Industries</title>
         <meta charset="utf-8"/>
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        <link rel="stylesheet" href="stlye.css">
+        <link rel="stylesheet" href="style.css">
+        <link rel="icon" href="pictures/chrisindustries.png"/>
         <script src="http://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
-        <>
     </head>
 
-        <body>
-        
-        </body>
+            <body>
+                <div class="heading">
+                <h1>Chris Industries</h1>
+                </div>
+                
+            </body>
 </html>
