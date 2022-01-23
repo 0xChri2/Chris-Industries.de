913a403d64ff5a042716c15dcab0ea231fe86f83
ImChri2 <chris@chris-industries.de>
2022-01-23 11:51:22 +0100

    Heading changes and added profil

--- chris-industries.de/index.php
+++ chris-industries.de/index.php
@@ -4,20 +4,26 @@
         <title>Chris Industries</title>
         <meta charset="utf-8"/>
         <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css">
         <link rel="stylesheet" href="style.css">
         <link rel="icon" href="pictures/chrisindustries.png"/>
         <script src="http://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
+        
     </head>
 
             <body>
-                <div class="heading">
+                <div class="heading">   
+                <div class="title">
                 <h1>
-                <img src="pictures/chrisindustries.png" width="5%" alt="chris-industries-logo" /> Chris Industries
+                    <img src="pictures/chrisindustries.png" width="5%" alt="chris-industries-logo" /> Chris Industries <a href="https://www.twitch.tv/imchri2"><img src="https://static.twitchcdn.net/assets/favicon-32-e29e246c157142c94346.png" alt="twitch logo"/></a>
                 </h1>
-                </div>
+                </div> </div>
+                
                 <div class="profil">
                 
+                <img src="pictures/ChrisBild Zoomed.jpeg" alt="ChrisBild" width="15%" height="15%"/>
+                <h1 style="padding-left: 15%">Chris</h1><br />
                 <h4>Hallo ich bin Chris lerne IT und bin besonders interessiert in Netzwerk Administration, Cyber Securtiy und Development. Nebenbei bin ich ein Autofreak der besonder JDM, Drift und 5 Zylinder mag.</h4>
                 </div>
             </body>
-</html>
+</html>3
