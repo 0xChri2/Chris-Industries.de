bc31d8ae8b964f44bbcea080a4c7e19d2565d07d
ImChri2 <chris@chris-industries.de>
2023-08-10 22:14:46 +0200

    changed ci logo to right size

--- Chris-Industries/src/components/navbar.vue
+++ Chris-Industries/src/components/navbar.vue
@@ -10,7 +10,7 @@
 <nav class="bg-white border-gray-200 px-2 sm:px-4 py-2.5 rounded dark:bg-black">
   <div class="container flex flex-wrap items-center justify-between mx-auto">
   <a href="https://chris-industries.de" class="flex items-center">
-      <img src="../assets/chrisindustrieslogo.png" width="50px" class="h-15 mr-3 sm:h-15 rounded-3xl" alt="Chris Industries" />
+      <img src="../assets/chrisindustrieslogo.png" style="width: 35px;" class="h-15 mr-3 sm:h-15 rounded-3xl" alt="Chris Industries" />
       <span class="self-center text-xl font-semibold whitespace-nowrap dark:text-white">Chris Industries</span>
   </a>
   <div class="flex md:order-2">
