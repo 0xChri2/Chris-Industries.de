996f829c3aed161459d6b3780aec0fa3be32a94b
ImChri2 <chris@chris-industries.de>
2023-10-06 20:42:55 +0200

    centered the menu

--- Chris-Industries/src/components/navbar.vue
+++ Chris-Industries/src/components/navbar.vue
@@ -13,7 +13,7 @@
       <img src="../assets/chrisindustrieslogo.png" style="width: 35px;" class="h-15 mr-3 sm:h-15 rounded-3xl" alt="Chris Industries" />
       <span class="self-center text-xl font-semibold whitespace-nowrap dark:text-white">Chris Industries</span>
   </a>
-  <div class="flex md:order-2">
+  <div class="flex md:order-2 justify-center space-x-6">
     <button type="button" data-collapse-toggle="navbar-search" aria-controls="navbar-search" aria-expanded="false" class="md:hidden text-gray-500 dark:text-gray-400 hover:bg-gray-100 dark:hover:bg-gray-700 focus:outline-none focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-700 rounded-lg text-sm p-2.5 mr-1" >
       <svg class="w-5 h-5" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z" clip-rule="evenodd"></path></svg>
       <span class="sr-only">Search</span>
